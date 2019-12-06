5c1b24c5c641fbdee769d73eed5c1b7dbcc9b28c
tcdejong <thijs.casper.de.jong@gmail.com>
2019-12-06 23:03:41 +0100

    speed optimization day 5

--- 2018/day5.py
+++ 2018/day5.py
@@ -7,42 +7,36 @@ with open(filePath) as file:
     challengeInput = file.read().strip()
 
 
-def partOne(polymer):
-    madeChanges = True
-    while madeChanges:
-        madeChanges = False
-        i = 0
-        while i < len(polymer) - 1:
-            this = polymer[i]
-            that = polymer[i + 1]
+def partOne(polymer, output=True):
+    i = 0
+    while i < len(polymer) - 1:
+        this = polymer[i]
+        that = polymer[i + 1]
 
-            if this.casefold() == that.casefold():
-                if (this.islower() and that.isupper()) or (this.isupper() and that.islower()):
-                    polymer = polymer[:i] + polymer[i+2:]
-                    madeChanges = True
-                    i -= 1
-            i += 1
+        if this.casefold() == that.casefold():
+            if (this.islower() and that.isupper()) or (this.isupper() and that.islower()):
+                polymer = polymer[:i] + polymer[i+2:]
+                i = max(0, i - 2)
+        i += 1
 
-    print("Remaining polymer length: " + str(len(polymer)))
+    if output:
+        print("Remaining polymer length: " + str(len(polymer)))
     return len(polymer)
 
 
 def partTwo(polymer):
     units = set(list(polymer.lower()))
-
     shortestLen = len(polymer)
 
     for unit in units:
         unit = str(unit)
         newPoly = str(polymer).replace(unit, "").replace(unit.upper(), "")
-
-        newLen = partOne(newPoly)
-
+        newLen = partOne(newPoly, False)
         if newLen < shortestLen:
             shortestLen = newLen
 
     print("Shortest polymer length: " + str(shortestLen))
 
 
-# partOne(challengeInput)
+partOne(challengeInput)
 partTwo(challengeInput)
