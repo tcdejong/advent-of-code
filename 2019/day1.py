3ed948cee825d59ec2fc2e760fc9c1075a663a0f
tcdejong <thijs.casper.de.jong@gmail.com>
2020-03-18 15:21:02 +0100

    day 1 refactor

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,123 +1,36 @@
-import math
-
-input = [143845,
-         86139,
-         53043,
-         124340,
-         73213,
-         108435,
-         126874,
-         131397,
-         85618,
-         107774,
-         66872,
-         94293,
-         51015,
-         51903,
-         147655,
-         112891,
-         100993,
-         143374,
-         83737,
-         145868,
-         144768,
-         89793,
-         124127,
-         135366,
-         94017,
-         81678,
-         102325,
-         75394,
-         103852,
-         81896,
-         148050,
-         142780,
-         50503,
-         110691,
-         117851,
-         137382,
-         92841,
-         138222,
-         128414,
-         146834,
-         59968,
-         136456,
-         122397,
-         147157,
-         83595,
-         59916,
-         75690,
-         125025,
-         147797,
-         112494,
-         76247,
-         100221,
-         63389,
-         59070,
-         97466,
-         91905,
-         126234,
-         76561,
-         128170,
-         102778,
-         82342,
-         131097,
-         51609,
-         148204,
-         74812,
-         64925,
-         127927,
-         79056,
-         73307,
-         78431,
-         88770,
-         97688,
-         103564,
-         76001,
-         105232,
-         145361,
-         77845,
-         87518,
-         117293,
-         110054,
-         135599,
-         85005,
-         85983,
-         118255,
-         103031,
-         142440,
-         140505,
-         99614,
-         69593,
-         69161,
-         78795,
-         54808,
-         115582,
-         117976,
-         148858,
-         84193,
-         147285,
-         89038,
-         92677,
-         106574
-         ]
-
-
-def calcFuel(input):
+def calcFuel(moduleMasses, part=1):
     fuel = 0
-    for el in input:
+    for el in moduleMasses:
         latest = calcModuleFuel(el)
         fuel += latest
 
-        while latest > 0:
+        while latest > 0 and part == 2:
             latest = calcModuleFuel(latest)
             fuel += latest
 
-    print(fuel)
+    return fuel
 
 
 def calcModuleFuel(mass):
-    return max(math.floor(mass / 3) - 2, 0)
+    return max(mass // 3 - 2, 0)
 
 
-calcFuel(input)
+# Read input
+with open("day1.txt") as file:
+    moduleMasses = [int(x) for line in file if len(x := line.strip()) > 0]
+
+# Part one provided test cases
+assert calcModuleFuel(12) == 2
+assert calcModuleFuel(14) == 2
+assert calcModuleFuel(1969) == 654
+assert calcModuleFuel(100756) == 33583
+
+print("Part 1:", calcFuel(moduleMasses))
+
+
+# Part two provided test cases
+assert calcFuel([14], 2) == 2
+assert calcFuel([1969], 2) == 966
+assert calcFuel([100756], 2) == 50346
+
+print("Part 2:", calcFuel(moduleMasses, 2))
