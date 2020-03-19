3e287a121de2204a85bb158340080aa5c3600ce9
tcdejong <thijs.casper.de.jong@gmail.com>
2020-03-19 18:30:51 +0100

    class based refactor

--- 2019/day3.py
+++ 2019/day3.py
@@ -1,39 +1,121 @@
-# This is a pretty poor execution. It's sloppy.
-# Next time I'd probably write a class for Nodes and Edges.
-# Some refactoring wouldn't be bad either
-
 import math
-import os
-import sys
 
-H = "h"
-V = "v"
+class Node: 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-inputPath = os.path.join(sys.path[0], "day3.txt")
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def manhattanToOrig(self):
+        return abs(self.x) + abs(self.y)
+
+    def isOnEdge(self, edge):
+        return edge.x1 <= self.x <= edge.x2 and edge.y1 <= self.y <= edge.y2
+        
+
+class Edge:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.x1 = min(start.x, end.x)
+        self.x2 = max(start.x, end.x)
+        self.y1 = min(start.y, end.y)
+        self.y2 = max(start.y, end.y)
+        self.vertical = (self.x1 == self.x2)
+
+        self._len = max(self.x2 - self.x1, self.y2 - self.y1)
 
 
-def partOne(ex=0):
-    stepsA, stepsB = parseInput(ex)
+    def __repr__(self):
+        return f"{self.start}=>{self.end}"
 
-    nodesA = createNodes(stepsA)
-    nodesB = createNodes(stepsB)
-
-    nearestManhattanDistance = math.inf
-    for i in range(0, len(nodesA) - 1):
-        edgeA = (nodesA[i], nodesA[i+1])
-
-        for j in range(0, len(nodesB) - 1):
-            edgeB = (nodesB[j], nodesB[j+1])
-
-            dist = intersectionDistance(edgeA, edgeB)
-
-            if dist < nearestManhattanDistance:
-                nearestManhattanDistance = dist
-
-    return nearestManhattanDistance
+    
+    def __len__(self):
+        return self._len
 
 
-def parseInput(ex=""):
+    def intersection(self, other):
+        if not isinstance(other, Edge):
+            raise RuntimeError(f"other must be an Edge, but was {type(other)}")
+
+        if self.vertical == other.vertical:
+            return None
+        
+        if self.vertical:
+            return other.intersection(self)
+
+        intersectX = other.x1
+        intersectY = self.y1
+
+        if not self.x1 < intersectX < self.x2 or not other.y1 < intersectY < other.y2:
+            return None
+
+        return Node(intersectX, intersectY)
+
+
+class Wire:
+    def __init__(self, edges):
+        self.edges = edges
+        self.top = self.right = self.bot = self.left = 0
+
+        for edge in edges:
+            self.left = edge.x1 if edge.x1 < self.left else self.left
+            self.right = edge.x2 if edge.x2 > self.right else self.right
+            self.bot = edge.y1 if edge.y1 < self.bot else self.bot
+            self.top = edge.y2 if edge.y2 > self.top else self.top
+
+
+    def __repr__(self):
+        return "".join(str(self.edges))
+
+
+    def insideBoundingBox(self, edge):
+        if edge.vertical:
+            return self.left < edge.x1 < self.right and (self.bot < edge.y1 < self.top or self.bot < edge.y2 < self.top)
+        else:
+            return self.bot < edge.y1 < self.top and (self.left < edge.x1 < self.right or self.left < edge.x2 < self.right)
+
+
+    def findIntersections(self, other):
+        intersections = set()
+
+        myPossibleEdges = [e for e in self.edges if other.insideBoundingBox(e)]
+        theirPossibleEdges = [e for e in other.edges if self.insideBoundingBox(e)]
+
+        for myEdge in myPossibleEdges:
+            for theirEdge in theirPossibleEdges:
+                intersection = myEdge.intersection(theirEdge)
+                if intersection:
+                    intersections.add(intersection)
+
+        return intersections
+
+
+    def stepsToTargets(self, targets):
+        steps = 0
+        stepDict = {}
+        
+        for edge in self.edges:    
+            for target in targets:
+                if target.isOnEdge(edge):
+                    stepDict[target] = steps + len(Edge(edge.start, target))
+            
+            steps += len(edge)
+
+        return stepDict
+
+        
+
+def readInput(ex=""):
+    """ 
+    Read steps from puzzle input or examples, 
+    Convert steps to Nodes, 
+    Convert nodes to Edges, 
+    then return two dictionaries of Edge objects split as vertical and horizontal edges. 
+    """
+
     stepsA = []
     stepsB = []
 
@@ -44,140 +126,77 @@ def parseInput(ex=""):
         stepsA = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(sep=",")
         stepsB = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(sep=",")
     else:
-        with open(inputPath) as rawData:
+        with open("day3.txt") as rawData:
             stepsA = rawData.readline().strip().split(sep=",")
             stepsB = rawData.readline().strip().split(sep=",")
 
-    return (stepsA, stepsB)
+    nodesA, nodesB = createNodes(stepsA), createNodes(stepsB)
+    wireA, wireB = createWire(nodesA), createWire(nodesB)
+
+    return (wireA, wireB)
+
 
 
 def createNodes(stepList):
-    nodes = [(0, 0)]
+        """ Convert list of instructions to list of sequential Node instances. """
+        orig = Node(0, 0)
+        nodes = [orig]
 
-    for step in stepList:
-        current = nodes[-1]
-        x, y = current[0], current[1]
-        dx, dy = 0, 0
+        for step in stepList:
+            x, y = nodes[-1].x, nodes[-1].y
+            dx, dy = 0, 0
 
-        dir = step[:1]
-        dist = int(step[1:])
+            dir = step[:1]
+            dist = int(step[1:])
 
-        if dir == "U":
-            dy = dist
-        elif dir == "R":
-            dx = dist
-        elif dir == "D":
-            dy = -dist
-        elif dir == "L":
-            dx = -dist
+            if dir == "U":
+                dy = dist
+            elif dir == "R":
+                dx = dist
+            elif dir == "D":
+                dy = -dist
+            elif dir == "L":
+                dx = -dist
 
-        # each adjacent node in the list is connected, ergo nodes[z] and nodes[z+1] form an edge
-        nodes.append((x + dx, y + dy))
-
-    return nodes
-
-
-def intersectionDistance(edgeA, edgeB, part2=False):
-    Ax1, Ax2 = edgeA[0][0], edgeA[1][0]
-    Ay1, Ay2 = edgeA[0][1], edgeA[1][1]
-    Bx1, Bx2 = edgeB[0][0], edgeB[1][0]
-    By1, By2 = edgeB[0][1], edgeB[1][1]
-
-    orA, orB = H, H
-    if Ax1 == Ax2:
-        orA = V
-
-    if Bx1 == Bx2:
-        orB = V
-
-    # assumes no touching corners: these are no wire *crossings*
-    if orA == orB:
-        return math.inf
-
-    if orA == H:
-        Hx1, Hx2, Hy = min(Ax1, Ax2), max(Ax1, Ax2), Ay1
-        Vy1, Vy2, Vx = min(By1, By2), max(By1, By2), Bx1
-    else:
-        Hx1, Hx2, Hy = min(Bx1, Bx2), max(Bx1, Bx2), By1
-        Vy1, Vy2, Vx = min(Ay1, Ay2), max(Ay1, Ay2), Ax1
-
-    if Hx1 < Vx < Hx2 and Vy1 < Hy < Vy2:
-        if part2:
-            return (Vx, Hy)
-
-        return abs(Vx) + abs(Hy)
-    else:
-        return math.inf
+            nodes.append(Node(x + dx, y + dy))
+        return nodes
 
 
-print(partOne())
+def createWire(nodeList):
+    """ Convert list of nodes to edges, then create and return edge list as Wire instance. """
+    edges = []
+    for i, toNode in enumerate(nodeList):
+        if i == 0:
+            continue
+
+        fromNode = nodeList[i-1]
+        edges.append(Edge(fromNode, toNode))
+
+    return Wire(edges)
+
+
+def partOne(ex=0):
+    wireA, wireB = readInput(ex)
+    intersections = wireA.findIntersections(wireB)
+    minManhattan = min([n.manhattanToOrig() for n in intersections])
+    return minManhattan
+
+assert partOne(1) == 159
+assert partOne(2) == 135
+    
+print(f"Part one: {partOne()}")
 
 
 def partTwo(ex=0):
-    stepsA, stepsB = parseInput(ex)
-
-    nodesA = createNodes(stepsA)
-    nodesB = createNodes(stepsB)
-
-    minSteps = math.inf
-    for i in range(0, len(nodesA) - 1):
-        edgeA = (nodesA[i], nodesA[i+1])
-
-        for j in range(0, len(nodesB) - 1):
-            edgeB = (nodesB[j], nodesB[j+1])
-
-            res = intersectionDistance(edgeA, edgeB, True)
-            if res != math.inf:
-                steps = countSteps(res, stepsA) + countSteps(res, stepsB)
-                if steps < minSteps:
-                    minSteps = steps
-    return minSteps
+    wireA, wireB = readInput(ex)
+    intersections = wireA.findIntersections(wireB)
+    stepsA, stepsB = wireA.stepsToTargets(intersections), wireB.stepsToTargets(intersections)
+    minSignalDelay = min([stepsA[i] + stepsB[i] for i in intersections])
+    
+    return minSignalDelay
 
 
-def edgeLength(edge):
-    x1, y1, x2, y2 = edge[0][0], edge[0][1], edge[1][0], edge[1][1]
-    if x1 == x2:
-        return abs(abs(y2) - abs(y1))
-    else:
-        return abs(abs(x2) - abs(x1))
+assert partTwo(1) == 610
+assert partTwo(2) == 410
 
-
-def countSteps(target, stepList):
-    current = (0, 0)
-    tx, ty = target[0], target[1]
-    steps = 0
-
-    for step in stepList:
-        x, y = current[0], current[1]
-        dx, dy = 0, 0
-
-        dir = step[:1]
-        dist = int(step[1:])
-
-        if dir == "U":
-            dy = dist
-        elif dir == "R":
-            dx = dist
-        elif dir == "D":
-            dy = -dist
-        elif dir == "L":
-            dx = -dist
-
-        if dir == "U" or dir == "D":
-            if x != tx:
-                steps += dist
-                current = (x, y+dy)
-            else:
-                steps += abs(ty - y)
-                return steps
-            pass
-        else:
-            if y != ty:
-                steps += dist
-                current = (x+dx, y)
-            else:
-                steps += abs(tx - x)
-                return steps
-
-
-print(partTwo())
+print(f"Part two: {partTwo()}")
