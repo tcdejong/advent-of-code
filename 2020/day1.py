11cdba764e193e09fb9b66005baa04fd8564d29f
tcdejong <thijs.casper.de.jong@gmail.com>
2020-12-01 10:26:21 +0100

    2020 day 1

---
+++ 2020/day1.py
@@ -0,0 +1,40 @@
+example = [
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456,
+]
+
+
+def part_one(expenses, target = 2020):
+    expense_set = set(expenses)
+
+    for a in expenses:
+        remainder = target - a
+        if remainder in expense_set:
+            return a * remainder
+
+
+def part_two(expenses):
+    target = 2020
+
+    for a in expenses:
+        remainder = target - a
+        res = part_one(expenses, remainder)
+
+        if res:
+            return a * res
+
+    
+def main():
+    with open("day1.txt") as file:
+        expenses = [int(line) for line in file.read().split()]
+
+    print("Part one:", part_one(expenses))
+    print("Part two:", part_two(expenses))
+
+
+if __name__ == '__main__':
+    main()
