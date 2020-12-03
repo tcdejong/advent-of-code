08b50610028c4b6d2a22925f6b92af857675225e
tcdejong <thijs.casper.de.jong@gmail.com>
2020-12-03 10:04:44 +0100

    2020 day 3 completed

---
+++ 2020/day3.py
@@ -0,0 +1,48 @@
+from math import prod
+
+def read_input():
+    with open("day3.txt") as file:
+        grid = [line.strip() for line in file.readlines()]
+
+    return grid
+
+
+def traverse_grid(dx, dy):
+    grid = read_input()
+    x, y = 0, 0
+    x_lim = len(grid[0])
+    y_lim = len(grid)
+
+    trees = 0
+
+    while y < y_lim:
+        trees += grid[y][x] == "#"
+                
+        x = (x + dx) % x_lim
+        y += dy
+
+    return trees
+
+
+def part_one():
+    print(traverse_grid(3,1))
+
+
+def part_two():
+    step_sizes = [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ]
+
+    trees = (traverse_grid(dx, dy) for (dx,dy) in step_sizes)
+
+    print(prod(trees))
+
+
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
