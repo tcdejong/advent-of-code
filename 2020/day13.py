fb727293248a4ae7ba7ef00c661d20c01b053689
tcdejong <thijs.casper.de.jong@gmail.com>
2021-07-18 22:23:00 +0200

    complete day 13 part 2

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,4 +1,4 @@
-from math import prod
+from __future__ import annotations
 
 
 def next_departure(search_time, bus_id):
@@ -8,54 +8,76 @@ def next_departure(search_time, bus_id):
 
 def part_one():
     search_time, bus_ids = read_input()
-
     departures = [(bus_id, next_departure(search_time, bus_id)) for bus_id in bus_ids]
-
     earliest = min(departures, key=lambda x: x[1])
 
     print(f'Part one: {earliest[0]} * {earliest[1]} = {earliest[0] * earliest[1]}')
 
 
-def part_two_bf():
-    _, raw_bus_ids = read_input(raw=True)
+def pair_two(id1, id2, t0, offset, phase=None):
+    """
+    Find first t where:
+        t >= t0
+        id1 departs at t
+        id2 departs at t+offset
 
-    # brute force: find t so that t+i % bus == 0 for all i != x
+    Take steps of size phase. Phase is the joint period of already matched buses.
+    If this is the first match, it's the interval (id) of bus 1
 
-    # ex1 = [7,13,'x','x',59,'x',31,19] # 1068781
-    # ex2 = [1789,37,47,1889] # 1202161486
+    This is dark magic.
+    """
+    t = t0
 
-    t = prod(int(i) for i in raw_bus_ids if i.isnumeric())
+    if not phase:
+        phase = id1
 
-    while True:
-        if all((t + i) % int(bus) == 0 for i, bus in enumerate(raw_bus_ids) if bus != 'x'):
-            break
-        
-        if t % 1_000_000 == 0:
-            print(f'{t/1_000_000=}...')
-        
-        t +=1 
+    while not (t % id1 == 0 and (t+offset) % id2 == 0):
+        t += phase
 
-    print(f'Part two: {t=}')
+    phase = phase * id2
+    return (t, phase)
 
 
 def part_two():
+    """
+    Given bus ids [7,13,'x','x',59,'x',31,19],
+    find t so that:
+        bus 7 departs at t
+        bus 13 at t+1
+        ...                 no constraint at t+2 (x)
+        ...                 at t+3 (x)
+        bus 59 at t+4
+        ...                 at t+5 (x)
+        bus 31 at t+6
+        bus 19 at t+7
+
+
+        Ergo, find t so that:
+        (t + 0) % 7 = 0
+        (t + 1) % 13 = 0
+        (t + 4) % 59 = 0
+        (t + 6) % 31 = 0
+        (t + 7) % 19 = 0
+
+        relation (t+x) % y = 0 and t % y = z?
+        (10+1) % 5 = 1
+        10 % 5 = 0
+
+        (9+1) % 5 = 0
+        9 % 5 = 4
+    """
     _, raw_bus_ids = read_input(raw=True)
-    # ex1 = [7,13,'x','x',59,'x',31,19] # 1068781
-    # ex2 = [1789,37,47,1889]           # 1202161486
+    bus_and_offset = [(int(bus), i) for i, bus in enumerate(raw_bus_ids) if bus != 'x']
 
-    bus_ids = [(i, int(bus)) for i, bus in enumerate(raw_bus_ids) if bus != 'x']
+    # pair other busses one by one with bus 1, but keep track of phase for valid solutions.
+    id1 = bus_and_offset[0][0]
+    t0 = 0
+    phase = 1
+    for id2,offset in bus_and_offset[1:]:
+        t0, phase = pair_two(id1, id2, t0, offset, phase)
 
-    # i, j = bus_ids[:2]
+    print('Part two:', t0)
 
-    # n = i[1]
-    # while True:
-    #     if n % i and (n+)
-    #     n += i
-
-    # t is valid if:
-    # (t + i) % id == 0 for each bus id
-
-    # print(f'Part two: {t=}')
 
 
 def read_input(file_path = "day13.txt", raw=False):
@@ -66,14 +88,5 @@ def read_input(file_path = "day13.txt", raw=False):
     bus_ids = lines[1].split(',') if raw else [int(i) for i in lines[1].split(',') if i.isnumeric()]
 
     return search_time, bus_ids
-
-
-# if __name__ == '__main__':
-    # part_one()
-    # part_two()
-    # visualizer()
-
-part_two()
-
 
 
