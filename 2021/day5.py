46dc4a8982e099cdc61103029a25afb5d1a852a4
T. C. de Jong <thijs.casper.de.jong@gmail.com>
2021-12-18 12:50:59 +0100

    day5p2 done

--- 2021/day5.py
+++ 2021/day5.py
@@ -10,45 +10,44 @@ def read_input() -> list[Line]:
 
     data = [Line(*map(int, groups)) for groups in re.findall(mask, raw)]
     return data
-    
-    
 
-def part_one() -> int:
-    lines = read_input()
-    lines_hv = [l for l in lines if l.x1 == l.x2 or l.y1 == l.y2]
-    grid = defaultdict(int)
 
-    for l in lines_hv:
-        x1, y1, x2, y2 = l
+def generate_points(l: Line) -> list[tuple[int, int]]:
+    x1, y1, x2, y2 = l
 
+    if x1 == x2 or y1 == y2:
         xs = range(x1, x2+1) if x1 < x2 else range(x2, x1+1)
         ys = range(y1, y2+1) if y1 < y2 else range(y2, y1+1)
-        points = [(x, y) for x in xs for y in ys]
+        return [(x, y) for x in xs for y in ys]
+    else:
+        dx = 1 if x1 <= x2 else -1
+        dy = 1 if y1 <= y2 else -1
+        points = list(zip(range(x1, x2+dx, dx), range(y1, y2+dy, dy)))
+        return points
 
+
+def calc_overlap(part: int) -> int:
+    lines = read_input()
+    if part == 1:
+        lines = [l for l in lines if l.x1 == l.x2 or l.y1 == l.y2]
+    grid = defaultdict(int)
+
+    for l in lines:
+        points = generate_points(l)
         for xy in points: 
             grid[xy] += 1
 
     return (len([xy for xy in grid.values() if xy > 1]))
 
 
-def part_two() -> int:
-    lines = read_input()
-    lines_d = [l for l in lines if l.x1 != l.x2 and l.y1 != l.y2]
-    grid = defaultdict(int)
+def part_one():
+    return calc_overlap(1)
 
-    for l in lines_d:
-        x1, y1, x2, y2 = l
-
-        xs = range(x1, x2+1) if x1 < x2 else range(x2, x1+1)
-        ys = range(y1, y2+1) if y1 < y2 else range(y2, y1+1)
-        points = [(x, y) for x in xs for y in ys]
-
-        for xy in points: 
-            grid[xy] += 1
-
-    return (len([xy for xy in grid.values() if xy > 1]))
+def part_two():
+    return calc_overlap(2)
 
 
 if __name__ == '__main__':
     print(f'Part one: {part_one()}')
-    # print(f'Part two: {part_two()}')
+    print(f'Part two: {part_two()}')
+
