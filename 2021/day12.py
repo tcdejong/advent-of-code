0a2d8c36bbe89b202c2ea408dc7465abe905a664
T. C. de Jong <thijs.casper.de.jong@gmail.com>
2021-12-19 20:35:46 +0100

    day 12 p2

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 def read_input(filename: str = "day12.txt"):
     with open(filename) as f:
@@ -16,32 +16,11 @@ def read_input(filename: str = "day12.txt"):
 def part_one():
     cave = read_input("day12.txt")
     path = ['start']
-
     paths = finish_path(path, cave)
-
     return len(paths)
 
 
-def finish_path(path: list[str], cave):
-    pos = path[-1]
-
-    if pos == "end":
-        return [path]
-
-    seen_small = set(x for x in path if x.islower())
-    possible_steps = set(cave[pos]) - seen_small
-
-    if len(possible_steps) == 0:
-        return
-
-    finished_paths = [finish_path([*path, step], cave) for step in possible_steps]
-    finished_paths = [p for p in finished_paths if p]
-    finished_paths = [p for inner in finished_paths for p in inner]
-
-    return finished_paths
-
-
-def finish_path(path: list[str], cave, part=1):
+def finish_path(path: list[str], cave: dict, part=1):
     pos = path[-1]
 
     if pos == "end":
@@ -50,6 +29,12 @@ def finish_path(path: list[str], cave, part=1):
     if part == 1:
         seen_small = set(x for x in path if x.islower())
         possible_steps = set(cave[pos]) - seen_small
+    else:
+        visit_counts = Counter(path)
+        can_visit_twice = all(visit_counts[step] <= 1 or step.isupper() for step in path[1:])
+        connected_caves = [p for p in cave[pos] if p != 'start']
+        possible_steps = [step for step in connected_caves if step.isupper() or visit_counts[step] == 0 or (visit_counts[step] == 1 and can_visit_twice)]
+        # possible_steps = cave[pos]
 
     if len(possible_steps) == 0:
         return
@@ -61,9 +46,11 @@ def finish_path(path: list[str], cave, part=1):
     return finished_paths
 
 
-
 def part_two():
-    pass
+    cave = read_input("day12.txt")
+    path = ['start']
+    paths = finish_path(path, cave, part=2)
+    return len(paths)
 
 
 if __name__ == '__main__':
