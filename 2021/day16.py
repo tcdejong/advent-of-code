6be302939dcbcd49a67d71e5fb6814d844e4ab1b
T. C. de Jong <thijs.casper.de.jong@gmail.com>
2022-02-24 23:35:11 +0100

    day16 p1 complete

--- 2021/day16.py
+++ 2021/day16.py
@@ -24,17 +24,20 @@ def read_packet(binstr: str):
     if len(binstr) < 6:
         raise ValueError('binstr too short!')
 
-    _, packet_type = read_version_and_id(binstr)
+    bin_version = binstr[:3]
+    bin_type = binstr[3:6]
+
+    packet_version = int(bin_version,2)
+    packet_type = int(bin_type, 2)
+
+    read_func = read_literal if packet_type == PTYPE_LITERAL else read_operator
     
-    if packet_type == PTYPE_LITERAL:
-        packet = read_literal(binstr)
-        return packet
+    packet, remainder = read_func(binstr, packet_version, packet_type)
     
-    return read_operator(binstr)
+    return packet, remainder
 
     
-def read_operator(binstr: str):
-    packet_version, packet_type = read_version_and_id(binstr)
+def read_operator(binstr, packet_version, packet_type):
     length_type_id = binstr[6]
     mode = 'numbits' if length_type_id == "0" else 'numpackets'
     type_length = 15 if mode == 'numbits' else 11
@@ -42,7 +45,9 @@ def read_operator(binstr: str):
     subpacket_len_bits = binstr[7:7+type_length]
     subpacket_len = int(subpacket_len_bits,2)
 
-    subpacket_bits = binstr[7+type_length:]
+    payload = binstr[7+type_length:]
+    numbits = 0
+    numpackets = 0
     processed = 0
     data = []
 
@@ -54,36 +59,24 @@ def read_operator(binstr: str):
     # print(subpacket_bits.rjust(len(binstr)), sep="")
 
     while processed < subpacket_len:
-        packet = read_packet(subpacket_bits)
+        packet, payload = read_packet(payload)
         data.append(packet)
 
-        if mode == 'numbits':
-            processed += len(packet.binstr)
-        elif mode == 'numpackets':
-            processed += 1
-        else:
-            raise ValueError()
+        numbits += len(packet.binstr)
+        numpackets += 1
 
-        subpacket_bits = subpacket_bits[len(packet.binstr):]
+        processed = numbits if mode == 'numbits' else numpackets
 
-        # [print('\t', d) for d in data]
         if processed > subpacket_len:
             print('Warning! processed > subpacket_len')
 
-    p = Packet(packet_version, packet_type, data, binstr)
+    packet_len = 7+type_length+numbits
+    p = Packet(packet_version, packet_type, data, binstr[:packet_len])
 
-    return p
-
-
-def read_version_and_id(binstr: str):
-    bin_version = binstr[:3]
-    bin_id = binstr[3:6]
-
-    return int(bin_version,2), int(bin_id, 2)
+    return p, payload
 
     
-def read_literal(binstr):
-    packet_version, packet_type = read_version_and_id(binstr)
+def read_literal(binstr, packet_version, packet_type):
     # print(f'Reading literal packet {packet_version=} {binstr=}')
     data_bits = binstr[6:]
     block_width = 5
@@ -102,7 +95,7 @@ def read_literal(binstr):
     length = 6 + i + block_width
     value = int(data,2)
 
-    return Packet(packet_version, packet_type, value, binstr[:length])
+    return Packet(packet_version, packet_type, value, binstr[:length]), binstr[length:]
 
 
 def sum_of_version_numbers(packet: Packet):
@@ -113,12 +106,16 @@ def sum_of_version_numbers(packet: Packet):
         return packet.version + sum(sum_of_version_numbers(p) for p in packet.value)
     
 
-def part_one(hex_str):
-    binstr = hexstr2binstr(hex_str)
-    packet = read_packet(binstr)
+def part_one(hex_str=False):
 
-    print('Part one packet:')
-    print(packet)
+    if not hex_str:
+        hex_str = read_input()
+
+    binstr = hexstr2binstr(hex_str)
+    packet, _ = read_packet(binstr)
+
+    # print('Part one packet:')
+    # print(packet)
 
     return sum_of_version_numbers(packet)
 
@@ -129,35 +126,37 @@ def part_two():
 
 
 if __name__ == '__main__':
-    # assert hexstr2binstr('D2FE28') == '110100101111111000101000'
-    # assert read_version_and_id('110100101111111000101000') == (6, 4)
+    assert hexstr2binstr('D2FE28') == '110100101111111000101000'
 
-    # binstr = hexstr2binstr("D2FE28")
-    # packet = read_packet(binstr)
-    # assert packet.value == 2021
+    binstr = hexstr2binstr("D2FE28")
+    packet, _ = read_packet(binstr)
+    assert packet.value == 2021
 
-    # binstr = hexstr2binstr("38006F45291200")
-    # packet = read_packet(binstr)
-    # assert len(packet.value) == 2
-    # assert len(packet.value[0].binstr) == 11
-    # assert packet.value[0].value == 10
-    # assert len(packet.value[1].binstr) == 16
-    # assert packet.value[1].value == 20
+    binstr = hexstr2binstr("38006F45291200")
+    packet, _ = read_packet(binstr)
+    assert len(packet.value) == 2
+    assert len(packet.value[0].binstr) == 11
+    assert packet.value[0].value == 10
+    assert len(packet.value[1].binstr) == 16
+    assert packet.value[1].value == 20
 
 
-    # binstr = hexstr2binstr("EE00D40C823060")
-    # packet = read_packet(binstr)
-    # assert len(packet.value) == 3
-    # assert packet.value[0].value == 1
-    # assert packet.value[1].value == 2
-    # assert packet.value[2].value == 3
+    binstr = hexstr2binstr("EE00D40C823060")
+    packet, _ = read_packet(binstr)
+    assert len(packet.value) == 3
+    assert packet.value[0].value == 1
+    assert packet.value[1].value == 2
+    assert packet.value[2].value == 3
 
-    # assert part_one("8A004A801A8002F478") == 16
+    assert part_one("8A004A801A8002F478") == 16
 
     # print('\n\n--------------------------------------\n\n')
 
     assert part_one("620080001611562C8802118E34") == 12
+    assert part_one("C0015000016115A2E0802F182340") == 23
+    assert part_one("A0016C880162017C3686B18A3D4780") == 31
+
     
-    # print(f'Part one: {part_one()}')
+    print(f'Part one: {part_one()}')
     # print(f'Part two: {part_two()}')
 
