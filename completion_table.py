bd21e2850684879ca1ae67c096b34c22d4536432
T. C. de Jong <thijs.casper.de.jong@gmail.com>
2023-07-10 20:35:32 +0200

    updated table marks

--- completion_table.py
+++ completion_table.py
@@ -1,6 +1,9 @@
 AOC_YEARS = [str(y) for y in range(2015, 2023)]
 AOC_DAYS = range(1,26)
 
+MARK_UNCOMPLETED = " "
+MARK_COMPLETED_P1 = "*"
+MARK_COMPLETED_P2 = "**"
 
 def print_table(data: dict):
     table_header = " | ".join(["Year ", *[str(d) if d > 9 else f" {d}" for d in AOC_DAYS]])
@@ -12,17 +15,17 @@ def print_table(data: dict):
 
 
 if __name__ == '__main__':
-    completion_data = {year: ["--" for _ in AOC_DAYS] for year in AOC_YEARS}
+    completion_data = {year: [MARK_UNCOMPLETED for _ in AOC_DAYS] for year in AOC_YEARS}
 
     # year: list[completed days]
     # 0 indexed, off by one
-    completed_p1_only = {
+    completed_p1 = {
         2021: [16]
     }
 
     # year: list[completed days]
     # 0 indexed, off by one
-    completed_p1p2 = {
+    completed_p2 = {
         2015: list(range(0, 8)),
         2016: list(range(0, 10)),
         2018: list(range(0, 7)),
@@ -31,12 +34,13 @@ if __name__ == '__main__':
         2021: list(range(0, 16)),
     }
 
-    for k, v in completed_p1p2.items():
+    for k, v in completed_p1.items():
         for i in v:
-            completion_data[str(k)][i] = "**"
+            completion_data[str(k)][i] = MARK_COMPLETED_P1
+    
+    for k, v in completed_p2.items():
+        for i in v:
+            completion_data[str(k)][i] = MARK_COMPLETED_P2
 
-    for k, v in completed_p1_only.items():
-        for i in v:
-            completion_data[str(k)][i] = "*"
 
     print_table(completion_data)
