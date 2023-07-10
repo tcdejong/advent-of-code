87e2af9adae723c725a8fbdc865fd954b06e7d5a
T. C. de Jong <thijs.casper.de.jong@gmail.com>
2023-07-10 20:32:39 +0200

    updated readme and completion table script

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Advent of Code (AoC) is an annual coding challenge hosted at [adventofcode.com](
 This repository contains my Python solutions for AoC. Its contents are the result of a self-directed learning process, as well as experimentation. Code inside is a mix of the two and not representative of what I consider "proper" code: these are scripts to solve one-off problems and therefore see no reuse, so I've focused on solving the puzzles rather than writing clean modules or a standardized codebase. 
 
 The puzzle descriptions and inputs can be found at [AoC's website](https://adventofcode.com/2019/events). They are not included in this repository given [the creator's comments about this practice](https://www.reddit.com/r/adventofcode/comments/e4mkjq/allowed_to_use_problem_text_as_github_readme/).
+
+# Completion progress
+
+Year  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25
+:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
+2015 | ** | ** | ** | ** | ** | ** | ** | ** | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | --
+2016 | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | --
+2017 | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | --
+2018 | ** | ** | ** | ** | ** | ** | ** | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | --
+2019 | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | --
+2020 | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | -- | -- | -- | -- | -- | -- | --
+2021 | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | ** | * | -- | -- | -- | -- | -- | -- | -- | --
+2022 | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | --
--- completion_table.py
+++ completion_table.py
@@ -1,133 +1,42 @@
-from typing import Union
-
-
-AOC_YEARS = [str(y) for y in range(2015, 2020)]
+AOC_YEARS = [str(y) for y in range(2015, 2023)]
 AOC_DAYS = range(1,26)
 
-MODES = {
-    "1": 'Single specific day',
-    "2": 'Range of consecutive days',
-    "3": 'All days, one by one',
-    None: 'End input.'
-}
 
-
-def choose_mode() -> Union[int, None]:
-    while True:
-        # Ask user for year of entry
-        print("Choose an input mode:")
-        [print(f"    {key} - {val}") for key,val in MODES.items()]
-
-        choice = input()
-
-        if choice == "":
-            return None
-
-        if choice in MODES:
-            return choice
-        else:
-            print(f"\nInvalid choice ({choice}), please try again.")
-            continue
-
-    
-
-def choose_year() -> str:
-    while True:
-        # Ask user for year of entry
-        print("Choose a year:")
-        [print(f"    {year}") for year in AOC_YEARS]
-        print("(Empty input to quit)")
-
-        choice = input()
-
-        if choice == "":
-            break
-
-        if choice in AOC_YEARS:
-            return choice
-        else:
-            print(f"\nInvalid year ({choice}), please try again.")
-            continue
-
-    return ""
-
-
-def enter_data(mode, current_data: list):
-    assert mode in MODES
-    assert mode != None
-    assert len(current_data) == 25
-
-
-
-
-def generate_completion_table(completion_dict: dict) -> str:
-    pass
-    # Verify input
-
-    # Generate headers
-
-    # Per key in dict, generate markdown table row displaying completion based on input
-
-
-def generate_completion_dict() -> dict:
-    "Function to generate a data structure containing the completion state"
-
-    data = {year: ["--" for _ in AOC_DAYS] for year in AOC_YEARS}
-    
-    while True:
-        print("Current data:")
-        print_data(data)
-
-        year = choose_year()
-
-        if year:
-            mode = choose_mode()
-            enter_data(mode, data[year])
-
-        else:
-            choice = input("Enter more data? y/n:")
-            if choice == "y":
-                continue
-            elif choice == "n":
-                break
-            else:
-                "Unrecognized entry, try again"
-
-
-
-    # Proces input, 
-    # Ask for more input
-
-    # Return dict
-    return data
-
-
-
-
-def make_table(data: dict):
-    header = " | ".join(["&nbsp;", *[str(d) if d > 9 else f" {d}" for d in AOC_DAYS]])
-    print(header)
-
-    sep = "---:|" * 25
-    print(sep)
+def print_table(data: dict):
+    table_header = " | ".join(["Year ", *[str(d) if d > 9 else f" {d}" for d in AOC_DAYS]])
+    print(table_header)
+    print(":---:|" * 26)
 
     for year,val in data.items():
         print(" | ".join([year, *val]))
 
 
 if __name__ == '__main__':
-    # main()
+    completion_data = {year: ["--" for _ in AOC_DAYS] for year in AOC_YEARS}
 
-    data = {year: ["--" for _ in AOC_DAYS] for year in AOC_YEARS}
-
-    completed = {
-        2015: list(range(0, 7)),
-        2018: list(range(0, 7)),
-        2019: list(range(0, 13)),
+    # year: list[completed days]
+    # 0 indexed, off by one
+    completed_p1_only = {
+        2021: [16]
     }
 
-    for k, v in completed.items():
-        for i in v:
-            data[str(k)][i] = "**"
+    # year: list[completed days]
+    # 0 indexed, off by one
+    completed_p1p2 = {
+        2015: list(range(0, 8)),
+        2016: list(range(0, 10)),
+        2018: list(range(0, 7)),
+        2019: list(range(0, 13)),
+        2020: list(range(0, 18)),
+        2021: list(range(0, 16)),
+    }
 
-    make_table(data)
+    for k, v in completed_p1p2.items():
+        for i in v:
+            completion_data[str(k)][i] = "**"
+
+    for k, v in completed_p1_only.items():
+        for i in v:
+            completion_data[str(k)][i] = "*"
+
+    print_table(completion_data)
