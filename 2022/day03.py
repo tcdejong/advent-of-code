55caeebbcac626209b17e3c8e916ed4649390de9
T. C. de Jong <thijs.casper.de.jong@gmail.com>
2023-07-11 11:31:19 +0200

    2022 day 3 part 1

---
+++ 2022/day03.py
@@ -0,0 +1,43 @@
+from string import ascii_lowercase, ascii_uppercase
+
+def read_input(filename: str = '2022/day03.txt'):
+    with open(filename) as f:
+        data = [line.strip() for line in f.readlines()]
+
+    return data
+
+
+def find_item_in_both_compartments(rucksack_contents):
+    items_per_compartment = len(rucksack_contents) // 2
+    items_comp1 = rucksack_contents[:items_per_compartment]
+    items_comp2 = rucksack_contents[items_per_compartment:]
+    assert len(items_comp1) == len(items_comp2)
+
+    shared_item = set(list(items_comp1)) & set(list(items_comp2))
+    assert len(shared_item) == 1
+
+    return shared_item.pop()
+
+
+def get_item_priority(item):
+    ordered_aasci = ascii_lowercase + ascii_uppercase
+    return 1 + ordered_aasci.find(item)
+
+
+def part_one(puzzle_input):
+    shared_items = [find_item_in_both_compartments(rucksack) for rucksack in puzzle_input]
+    item_priorities = [get_item_priority(item) for item in shared_items]
+    return sum(item_priorities)
+
+
+def part_two(puzzle_input):
+    pass
+
+
+if __name__ == '__main__':
+    ex1 = read_input('2022/day03ex1.txt')
+    assert part_one(ex1) == 157
+
+    puzzle_input = read_input()
+    print(f'Part one: {part_one(puzzle_input)}')
+    # print(f'Part two: {part_two(puzzle_input)}')
