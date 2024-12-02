36d91ec83a0fcd59dd5ffa1e0dc8831a6ee0c19d
Casper de Jong <casper.dejong@dimaros.nl>
2024-12-02 11:50:32 +0100

    typing fixes

--- 2015/day02/day02.py
+++ 2015/day02/day02.py
@@ -1,14 +1,16 @@
-def paper_required(w,h,l):
-    return 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
+def paper_required(w: int,h: int,l: int):
+    area = (2*l*w) + (2*w*h) + (2*h*l)
+    slack = min(l*w, w*h, h*l)
+    return area + slack
 
 
-def ribbon_required(w,h,l):
+def ribbon_required(w: int,h: int,l: int):
     min_perimeter = 2*(w+h+l)-2*max(w,h,l)
     volume = w*h*l
     return min_perimeter + volume
 
 
-def part_one():
+def part_one(puzzle_input: list[tuple[int, ...]]):
     res = 0
     for dims in puzzle_input:
         w,h,l = dims
@@ -17,7 +19,7 @@ def part_one():
     print(f"Pt1: {res}")
 
 
-def part_two():
+def part_two(puzzle_input: list[tuple[int, ...]]):
     res = 0
     for dims in puzzle_input:
         w,h,l = dims
@@ -28,9 +30,12 @@ def part_two():
 
 if __name__ == "__main__":
     # convert puzzle input into list of (w,h,l) integer tuples
-    with open("day2.txt") as file:
-        puzzle_input = [line.strip().split("x") for line in file.readlines()[:-1]]
+    with open("input.txt") as file:
+        puzzle_input = [line.strip().split("x") for line in file.readlines()]
         puzzle_input = [tuple(map(int, _)) for _ in puzzle_input]
 
-    part_one()
-    part_two()
+    assert paper_required(2,3,4) == 58
+    assert paper_required(1,1,10) == 43
+
+    part_one(puzzle_input)
+    part_two(puzzle_input)
